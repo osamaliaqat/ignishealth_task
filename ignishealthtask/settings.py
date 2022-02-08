17ff2ab48778f5beeba1a183d83e5844bf4e889e
Osama Liaqat <38002696+osamaliaqat@users.noreply.github.com>
2022-02-08 11:50:48 +0500

    updates

--- ignishealthtask/settings.py
+++ ignishealthtask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w3ip9t_iolitf_7n4umvzsdcdmx^ua(y!%r!nir8n-56crm)sy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
